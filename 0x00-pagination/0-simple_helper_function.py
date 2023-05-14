ff678617867a7776497a3f4d8814ccd9ce09efa3
beba2123 <antenehbeba@gmail.com>
2023-05-14 09:31:37 +0300

    intial 0x00-pagination commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,9 +9,7 @@
 
     Page numbers are 1-indexed, i.e. the first page is page 1.
 '''
-
 from typing import Tuple
-
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     ''' Return tuple containing pagination start index and end index. '''
