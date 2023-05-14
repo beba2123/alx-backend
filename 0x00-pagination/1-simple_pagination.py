c84dc6713c03b8512893e17779bf07b0d42d5d84
beba2123 <antenehbeba@gmail.com>
2023-05-14 17:09:05 +0300

    1-simple updated commit

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
-"""Discription:> implement a method name get_page that takes two arguments page
-            with default value 1 and page size with default value 10.
-            use assert to verify both arguments are integer greater than zero
-            and also use index_range to find the correct indexes to paginate
-            dataset.
 """
-
+0-simple_helper_function module
+"""
 import csv
 import math
-from typing import List
-
-index_range = __import__('0-simple_helper_function.py').index_range
-
-# this is for importing index_range function from 0-simple _helper_function.py
+from typing import List, Tuple
 
 
-class server:
-    """server class to paginate a database of popular baby names"""
-    DATA_FILE = "popular baby names.csv"
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -34,13 +36,15 @@ class server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        ''' Return page of dataset. '''
-        assert isinstance(page, int) and isinstance(page_size, int)
+        """
+        Takes the page index and page_size and returns the page contents paged
+        on the indices form the dataset
+        """
+        assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
 
         start, end = index_range(page, page_size)
         dataset = self.dataset()
-
         if start >= len(dataset):
             return []
         return dataset[start:end]
