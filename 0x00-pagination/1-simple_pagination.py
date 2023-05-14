2dca8d20f15195ea693f7084cb7c02baee98babb
beba2123 <antenehbeba@gmail.com>
2023-05-14 17:23:59 +0300

    1-simple updated commit

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,12 +8,8 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Returns a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
-    """
-    return ((page - 1) * page_size, page * page_size)
+    '''Return tuple containing pagination start index and end index.'''
+    return ((page_size * (page - 1)), page_size * page)
 
 
 class Server:
