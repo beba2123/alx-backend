c38156fb5aacada58fa44de73dab9ba236dbfaaa
beba2123 <antenehbeba@gmail.com>
2023-05-14 10:15:32 +0300

    intiall update 0-simple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,6 +10,7 @@
 '''
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    ''' Return tuple containing pagination start index and end index. '''
+    '''Return tuple containing pagination start index and end index.'''
     return ((page_size * (page - 1)), page_size * page)
