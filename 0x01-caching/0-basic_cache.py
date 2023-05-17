f2af5af6f3d9c6df6d4e6fe84c6bcedf3cdf4614
beba2123 <antenehbeba@gmail.com>
2023-05-17 20:54:57 +0300

    updated commit 0-basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,25 +8,24 @@ Create a class BasicCache that inherits from BaseCaching
 from base_caching import BaseCaching
 
 
-class BasiCache(BaseCaching): #inherit BaseCaching from base_Caching..
+class BasiCache(BaseCaching):
     """Base Cache"""
-    
+
     def __init__(self):
         super().__init__()
-    
+
     def put(self, key, item):
-        """assign to the dictionary self.cache_data 
+        """assign to the dictionary self.cache_data
              the item value for the key key
         """
-        if(key is not None and item is not None):
+        if (key is not None and item is not None):
             self.cache__data[key] = item
-    
+
     def get(self, key, item):
-        """
-        Must return the value in self.cache_data 
+        """ Must return the value in self.cache_data
            linked to key
         """
-        if(key is not None and item is not None):
+        if (key is not None and item is not None):
             return self.cache__data[key]
-        
+
         return None
