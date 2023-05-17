c07f6682080f6b8c90a48fae89ee3d09c3b34178
beba2123 <antenehbeba@gmail.com>
2023-05-17 21:06:04 +0300

    updated commit 0-basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -25,6 +25,6 @@ class BasicCache(BaseCaching):
         """ Must return the value in self.cache_data
            linked to key
         """
-        if (key is not None or key not in self.cache_data.keys()):
+        if (key is None or key not in self.cache_data.keys()):
             return None
         return self.cache_data[key]
